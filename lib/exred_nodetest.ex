9ab4e161808793bbc067283b38e17b8b8e961fcf
Zsolt <zsolt.erl@gmail.com>
2019-01-26 20:49:51 -0500

    fixes in moduledoc

--- lib/exred_nodetest.ex
+++ lib/exred_nodetest.ex
@@ -5,16 +5,18 @@ defmodule Exred.NodeTest do
   See example usage below. The `module` argument is the node module that will be tested.
 
   start_node/0 will start the node. It will add the :pid and :node keys to the test context.
+
   `:pid` is the process id of the running node GenServer
+
   `:node` is the node attributes map:
 
         %{
-          module :: atom,
-          id :: atom
-          name :: string,
-          category :: string,
-          info :: string,
-          config :: map,
+          module        :: atom,
+          id            :: atom
+          name          :: string,
+          category      :: string,
+          info          :: string,
+          config        :: map,
           ui_attributes :: map
         }
 
@@ -23,18 +25,18 @@ defmodule Exred.NodeTest do
   ## Example
   (this would go into a test file)
 
-    defmodule Exred.Node.MynodeTest do
-      use ExUnit.Case
-      use Exred.NodeTest, module: Exred.Node.Mynode
+      defmodule Exred.Node.MynodeTest do
+        use ExUnit.Case
+        use Exred.NodeTest, module: Exred.Node.Mynode
 
-      setup_all do
-        Exred.NodeTest.start_node
-      end
+        setup_all do
+          start_node()
+        end
 
-      test "has pid", context do
-        assert is_pid(context.pid)
+        test "has pid", context do
+          assert is_pid(context.pid)
+        end
       end
-    end
 
   """
 
